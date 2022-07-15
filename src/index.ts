f8c908238d3813597e2adfb4ed185e350662f574
dsd <yyyyyy>
2022-07-15 11:04:01 +0800

    add custom name

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,17 @@ type PersistOptions = {
   log?: boolean;
   // 是否开启持久化存储
   enabled?: boolean;
+  // 自定义名称
+  key?: string;
   // A single store needs to store part of the key-value, the default is to store all
-  keys?: string[]
+  keys?: string[];
   // Using Storage Types, default is localStorage
   storage?: Storage;
   //  Encrypt key, if not passed then not encrypted
   encryptionKey?: string;
   // Whether to retain data after component destruction
-  detached?: boolean
-}
+  detached?: boolean;
+};
 
 declare module 'pinia' {
   export interface DefineStoreOptionsBase<S, Store> {
@@ -30,26 +32,31 @@ const stateEncrypt = (state: StateTree & PiniaCustomStateProperties, key?: strin
   return key ? AES.encrypt(JSON.stringify(state), key).toString() : JSON.stringify(state);
 }
 
-const storageSet = (store: Store, storage: Storage, encryptionKey?: string, keys?: string[]) => {
+const storageSet = (
+  store: Store,
+  storage: Storage,
+  key: string,
+  encryptionKey?: string,
+  keys?: string[],
+) => {
   if (!keys) {
     const state = stateEncrypt(store.$state, encryptionKey);
-    storage.setItem(store.$id, state);
+    storage.setItem(key, state);
   } else {
     const state: Record<string, any> = {};
-    keys.forEach(key => {
+    keys.forEach((key) => {
       const value = store.$state[key];
       if (value !== undefined && value !== null) {
         state[key] = store.$state[key];
       } else {
-        console.warn('Persist key not found', key, store.$id);
+        console.warn("Persist key not found", key, store.$id);
       }
     });
-    storage.setItem(store.$id, stateEncrypt(state, encryptionKey)
-    );
+    storage.setItem(key, stateEncrypt(state, encryptionKey));
   }
-}
+};
 
-const storageSync = (store: Store, storage: Storage, oldState: string | null, encryptionKey?: string, keys?: string[]) => {
+const storageSync = (store: Store, storage: Storage, oldState: string | null, key: string, encryptionKey?: string, keys?: string[],) => {
   if (oldState) {
     let stateObj: Record<string, any>
     if (encryptionKey) {
@@ -74,7 +81,7 @@ const storageSync = (store: Store, storage: Storage, oldState: string | null, en
       })
     }
   } else {
-    storageSet(store, storage, encryptionKey, keys);
+    storageSet(store, storage, key, encryptionKey, keys);
   }
 }
 
@@ -84,20 +91,21 @@ export function usePersist({store, options: {persist}}: PiniaPluginContext) {
       console.warn('Persist keys is String[]', store.$id);
     }
 
+    const key = persist?.key || store.$id
     const keys = persist?.keys
     const storage = persist?.storage || localStorage;
     const encryptionKey = persist?.encryptionKey
 
     try {
-      const oldState = storage.getItem(store.$id);
-      storageSync(store, storage, oldState, encryptionKey, keys);
+      const oldState = storage.getItem(persist.key || store.$id);
+      storageSync(store, storage, oldState, key, encryptionKey, keys);
     } catch (error) {
       console.error('Persist error', error);
     }
 
     store.$subscribe(() => {
       console.log('Persist update', store.$state);
-      storageSet(store, storage, encryptionKey, keys);
+      storageSet(store, storage, key, encryptionKey, keys );
     }, {
       detached: persist?.detached || true,
       deep: true,
